b370e6f095d111450180feb008f38cd2a33d40c6
Igor Shtr <igor.shtr@gmail.com>
2020-04-27 16:02:12 +0300

    Updates

---
+++ precache-manifest.f138a5c6e2b23a73a96626442b6f75de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3904ebf9b0551240a45d6f4a6712189b",
+    "url": "/TestPekemons/index.html"
+  },
+  {
+    "revision": "0359efc5260ee5d8512f",
+    "url": "/TestPekemons/static/css/main.bda32a4c.chunk.css"
+  },
+  {
+    "revision": "7a8c60dda4da635829d4",
+    "url": "/TestPekemons/static/js/2.88b2d389.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TestPekemons/static/js/2.88b2d389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0359efc5260ee5d8512f",
+    "url": "/TestPekemons/static/js/main.49975556.chunk.js"
+  },
+  {
+    "revision": "fe4314d449731a4c128a",
+    "url": "/TestPekemons/static/js/runtime-main.dc9fc912.js"
+  }
+]);
