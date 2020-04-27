14e3ea00bf7a06c50b1996bf805f20fd3a979db1
Igor Shtr <igor.shtr@gmail.com>
2020-04-27 15:17:32 +0300

    Updates

---
+++ precache-manifest.f63dd0b3e303a2723ed7f69821b22466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808733b152ca7d835c198eb2294eff42",
+    "url": "/pokemons/index.html"
+  },
+  {
+    "revision": "3f9641051e3fbcf4071e",
+    "url": "/pokemons/static/css/main.bda32a4c.chunk.css"
+  },
+  {
+    "revision": "22a91be68b2fff21035b",
+    "url": "/pokemons/static/js/2.27fb7abb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemons/static/js/2.27fb7abb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f9641051e3fbcf4071e",
+    "url": "/pokemons/static/js/main.d111d131.chunk.js"
+  },
+  {
+    "revision": "3a2fa96ffcd815ddceeb",
+    "url": "/pokemons/static/js/runtime-main.1488e3f6.js"
+  }
+]);
